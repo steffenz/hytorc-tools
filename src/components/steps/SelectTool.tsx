f4d8f436775fc9b42eb431aa81d4eacd4280965c
Steffen Martinsen <steffen@steffenmedia.no>
2020-02-22 16:44:05 +0100

    Removed unused imports

--- src/components/steps/SelectTool.tsx
+++ src/components/steps/SelectTool.tsx
@@ -2,7 +2,6 @@ import React, { useContext } from "react";
 import { Tool } from '../../types';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
-
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
@@ -10,10 +9,7 @@ import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import { useTranslation } from 'react-i18next';
-
-
 import CalculatorContext from './../context/CalculatorContext';
-
 
 export default() => {    
     
@@ -48,7 +44,6 @@ export default() => {
     }
 
     const filteredTools = getToolsWithFilteredModelList(tools, torque);
-
 
     return(
         <React.Fragment>
